91ff4b13c1765872a2edfc32ca495ad3cf40cba4
Rahul Rambhatla <128779420+Rambhatla-Rahul@users.noreply.github.com>
2024-12-22 03:47:56 +0530

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='mt-'>
-      <img src='../../../public/assets/intro.jpg' className='w-full h-[800px]'/>
+      <img src='../../../public/assets/intro.jpg' className='w-full h-[830px]'/>
     </div>
   )
 }
